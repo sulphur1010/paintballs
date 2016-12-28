2b689160b1d84ec59c17b2e9aa0f16f5bb686cc8
Sulphur1010 <mustaphaalade@gmail.com>
2016-12-29 00:51:46 +0100

    fix friendly-id

--- paintball/app/models/member.rb
+++ paintball/app/models/member.rb
@@ -8,19 +8,9 @@ class Member < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
 
-    friendly_id :name, :use => :slugged 
-
-    def should_generate_new_friendly_id?
-    	 slug.nil? || name_changed? || new_record?
- 	end
+   
 
     # Try building a slug based on the following fields in
   	# increasing order of specificity.
-    def slug_candidates
-    [
-       :name,
-      [:name, :alias]
-    ]
-   end
-
+   
 end
--- paintball/app/models/team.rb
+++ paintball/app/models/team.rb
@@ -3,4 +3,9 @@ class Team < ActiveRecord::Base
  has_many :members, :through => :joint_member_teams
  mount_uploader  :logo, LogoUploader
  mount_uploader  :banner, BannerUploader
+
+
+    extend FriendlyId
+    friendly_id :name, use: :slugged
 end
+
